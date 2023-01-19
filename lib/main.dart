1c38c221c7fdf2ab4a7fae5f4de96faf85159a0d
Vivek Ghosh <vivekghosh165@gmail.com>
2023-01-19 19:24:13 +0530

    ntg

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Page'),
+      home: const MyHomePage(title: 'Flutter'),
     );
   }
 }
